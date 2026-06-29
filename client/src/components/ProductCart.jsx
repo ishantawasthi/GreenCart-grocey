60c5d8149444dbbd9606fd9825805462d1aeba68
Ishant Awasthi <ishantawasthi350@gmail.com>
2026-06-29 13:39:28 +0530

    responsive ui

--- client/src/components/ProductCart.jsx
+++ client/src/components/ProductCart.jsx
@@ -16,28 +16,26 @@ const ProductCart = ({product}) => {
                       window.scrollTo(0, 0); // ✅ scrolls to top
              }}
 
-        className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-40 max-w-50 w-full ">
+          className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white w-full">
             <div className="group cursor-pointer flex items-center justify-center  px-2">
                 <img className="group-hover:scale-105 transition max-w-26 md:max-w-36 " src={product.image[0]} alt={product.name} />
             </div>
             <div className="text-gray-500/60 text-sm ">
                 <p>{product.category}</p>
                 <p className="text-gray-700 font-medium text-lg truncate w-full">{product.name}</p>
-                <div className="flex items-center gap-0.5">
-                    {Array(5).fill('').map((_, i) => (
-                      
-                        <img key={i} src={i<4 ? assets.star_icon : assets.star_dull_icon }  
-                        alt="" className="md:w-3.5 w3"/>
-                
-                        ) 
-                    )}
-                    <p>(4)</p>
-                </div>
-                <div className="flex items-end justify-between mt-3">
-                    <p className="md:text-xl text-base font-medium text-primary">
-                        {currency}{product.offerPrice} {" "} <span className="text-gray-500/60 md:text-sm text-xs line-through"> {currency}{product.price}</span>
-                    </p>
-                    <div onClick={(e)=>{e.stopPropagation();}} className="text-primary">
+                      <div className="flex items-center gap-0.5 flex-wrap">
+    {Array(5).fill('').map((_, i) => (
+        <img key={i} src={i<4 ? assets.star_icon : assets.star_dull_icon }  
+        alt="" className="w-2.5 md:w-3.5 flex-shrink-0"/>
+    ))}
+    <p className="text-xs text-gray-500 ml-0.5 flex-shrink-0">(4)</p>
+</div>
+                <div className="flex flex-col gap-2 sm:flex-row sm:items-end sm:justify-between mt-3">
+    <div className="flex flex-col">
+        <p className="md:text-xl text-base font-medium text-primary">{currency}{product.offerPrice}</p>
+        <p className="text-gray-500/60 md:text-sm text-xs line-through">{currency}{product.price}</p>
+    </div>
+    <div onClick={(e)=>{e.stopPropagation();}} className="text-primary">
                         {!cartItems[product._id] ? (
                             <button className="flex items-center justify-center gap-1
                              bg-primary/10 border border-primary/14
